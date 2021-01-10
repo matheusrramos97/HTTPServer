6b489978a2799dc23f27399c2918cef860fc68c4
Matheus Ribeiro <matheusrramos97@gmail.com>
2021-01-10 12:57:36 -0300

    first commit

---
+++ src/modules/mailer.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const nodemailer = require('nodemailer')
+const hbs = require('nodemailer-express-handlebars')
+
+const {service, user, pass} = require('../config/mail.json')
+
+var transport = nodemailer.createTransport({
+    service,
+    auth: { user, pass }
+})
+
+transport.use('compile', hbs({
+    viewEngine: {
+      defaultLayout: undefined,
+      partialsDir: path.resolve('./src/resources/mail/')
+    },
+    viewPath: path.resolve('./src/resources/mail/'),
+    extName: '.html',
+  }));
+
+module.exports = transport
